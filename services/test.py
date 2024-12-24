dcb29313d313686ee747993bd84c9229160f7bbd
mikehansonn <90873556+mikehansonn@users.noreply.github.com>
2024-12-24 12:08:27 -0500

    a

--- services/test.py
+++ services/test.py
@@ -3,16 +3,15 @@ from typing import Tuple
 
 
 current_time = datetime.now(timezone.utc)
-print(current_time)
 
-if current_time.weekday() != 4:
+if current_time.weekday() != 1:
     print(False)
     
-activation_time = time(1, 00, tzinfo=timezone.utc)
+activation_time = time(6, 10, tzinfo=timezone.utc)
 current_day_activation = datetime.combine(
     current_time.date(),
     activation_time
 )
 
 time_difference = abs((current_time - current_day_activation).total_seconds())
-print(time_difference <= 1000)
+print(time_difference <= 120)
